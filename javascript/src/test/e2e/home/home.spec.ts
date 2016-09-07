e1290a435a94f71ed5cd4c0f4b606ee50ffc826d
Jonathan <poisson.jonathan777@gmail.com>
2016-09-07 18:47:16 -0400

    Added simple e2e test

--- javascript/src/test/e2e/home/home.spec.ts
+++ javascript/src/test/e2e/home/home.spec.ts
@@ -1,12 +1,18 @@
 describe('Home page', () => {
-    beforeAll(done => {
-        browser.get('/')
-        .then(done);
-    });
+  beforeAll(done => {
+    browser.get('/')
+      .then(done);
+  });
 
-    it('should have image', () => {
-        browser.sleep(1000);
-        let ng2Img = element(by.css('img'));
-        expect(ng2Img.isDisplayed()).toBeTruthy();
-    });
+  it('should have a kitten image', () => {
+    browser.sleep(1000);
+    let ng2Img = element(by.css('img'));
+    expect(ng2Img.isDisplayed()).toBeTruthy();
+  });
+
+  it('should have a title above the kitten image that is hidden', () => {
+    browser.sleep(1000);
+    let ng2Title = element(by.className('topBar'));
+    expect(ng2Title.isDisplayed()).toBeFalsy();
+  });
 });
