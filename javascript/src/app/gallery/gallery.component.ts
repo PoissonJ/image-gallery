0f7160d1bd80c1320f3db7c6e5a1a4188adca596
Jonathan <poisson.jonathan777@gmail.com>
2016-09-07 17:49:29 -0400

    Inital gallery component creation

---
+++ javascript/src/app/gallery/gallery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'as-gallery',
+  templateUrl: 'app/gallery/gallery.html',
+  styleUrls: [
+      'app/gallery/gallery.css'
+  ]
+})
+export class GalleryComponent {
+}
---
+++ javascript/src/app/gallery/gallery.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { GalleryComponent } from './index';
+
+@NgModule({
+  declarations: [
+      GalleryComponent
+  ],
+  exports: [
+      GalleryComponent
+  ]
+})
+export class HomeModule {
+}
